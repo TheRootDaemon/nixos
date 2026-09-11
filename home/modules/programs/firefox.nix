0494a8c06312f6623c0f977ffc13410d9a8dfb95
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-09-11 15:51:23 +0530

    refactor: Add some options for firefox

--- home/modules/programs/firefox.nix
+++ home/modules/programs/firefox.nix
@@ -12,17 +12,32 @@
       settings = {
         "browser.startup.page" = 1;
 
-        "full-screen-api.warning.delay" = 0;
-        "full-screen-api.warning.timeout" = 0;
-
+        # disables firefox's recomendations
         "browser.discovery.enabled" = false;
-        "browser.startup.homepage_override.mstone" = "ignore";
+
+        # hides sponsered contents in the new tab
         "browser.newtabpage.activity-stream.showSponsored" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
+        # prevent firefox from showing the "What's new"
+        "browser.startup.homepage_override.mstone" = "ignore";
+
+        # disable the pop-up when entering fullscreen
+        "full-screen-api.warning.delay" = 0;
+        "full-screen-api.warning.timeout" = 0;
       };
+    };
+  };
+
+  # set firefox as the default application
+  # for viewing/reading the file types below
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
     };
   };
 }
