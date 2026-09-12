56d44e70f19920b4171725def6d7f7216ea5f32b
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-09-12 23:02:04 +0530

    refactor: Fix issues with opencode

--- home/modules/programs/opencode.nix
+++ home/modules/programs/opencode.nix
@@ -1,3 +1,23 @@
-{dotfiles, ...}: {
-  home.file.".config/opencode".source = "${dotfiles}/.config/opencode";
+{
+  dotfiles,
+  pkgsUnstable,
+  ...
+}: {
+  # prefer latest releases
+  home.packages = with pkgsUnstable; [
+    opencode
+  ];
+
+  # symlinking the parent directory can cause conflicts
+  # when OpenCode tries to write the files within it
+  #
+  # the parent symlink would point to a immutable nix store,
+  # so when OpenCode tries to perform writes causing errors
+  home.file = {
+    ".config/opencode/agents".source = "${dotfiles}/.config/opencode/agents";
+    ".config/opencode/skills".source = "${dotfiles}/.config/opencode/skills";
+
+    ".config/opencode/opencode.jsonc".source = "${dotfiles}/.config/opencode/opencode.jsonc";
+    ".config/opencode/tui.jsoc".source = "${dotfiles}/.config/opencode/tui.jsonc";
+  };
 }
